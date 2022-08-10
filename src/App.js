6299526dfffd5413b1dd621391ee18cb7f409b6d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 19:14:07 -0700

    add import statement for Navbar and included navbar tag in return()

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
