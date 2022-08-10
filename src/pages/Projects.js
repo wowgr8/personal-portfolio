9c08b73f2af649a28dac9de1197422cf4e2d8705
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-10 13:19:58 -0700

    add import statement for styling and project gifs.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import ProjectItem from '../components/ProjectItem';
+import CAMPY from '../assets/gif/CAMPY.gif';
+import CatFish from '../assets/gif/CatFish.gif';
+import '../styles/Projects.css';
+
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1> My Personal Projects</h1>
+      <div className='projectList'>
+        <ProjectItem name='Campy' image={CAMPY} />
+        <ProjectItem name='CatFish' image={CatFish} />
+      </div>
+    </div>
   )
 }
 
