8f51f2cb91d5a5028f21df8fd523865dbf77046a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-10 13:47:46 -0700

    add func component with useParams import to display project by id

---
+++ src/pages/ProjectDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function ProjectDisplay() {
+  const { id } = useParams();
+  return (
+    <div>{id}</div>
+  )
+}
+
+export default ProjectDisplay;
