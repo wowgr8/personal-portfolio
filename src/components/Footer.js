73710a2a3e1ac0e6b7d33618d59131d942376a20
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 21:17:15 -0700

    add email icon and link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
 import '../styles/Footer.css'
 
 function Footer() {
@@ -8,6 +9,7 @@ function Footer() {
       <div className='socialMedia'>
         <a href='https://github.com/wowgr8'><BsGithub /></a>
         <a href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
+        <a href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
       </div>
       <p> &copy; 2022 github.com/wowgr8</p>
     </div>
