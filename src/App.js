85f0c35079f0f311481abb3303721b4fe277a37e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 18:58:07 -0700

    add import statement for react-router-dom and home, experience, and projects components. Created Routes for each component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
