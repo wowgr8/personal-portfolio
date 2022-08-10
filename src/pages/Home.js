8e7df5958516f8b0a4b11e2e16be6e935b4cf32d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 21:26:11 -0700

    add import statement for css styling and updated placeholder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
+import '../styles/Home.css';
 
 function Home() {
   return (
     <div className='home'>
       <div className='about'>
-        <h2>Hello, this is an about me placeholder</h2>
+        <h2>Howdy, I'm Cesar</h2>
         <div className='prompt'>
           <p> A driven Junior web developer pivoting from a career in customer service. Motivated to apply my interpersonal skills and passion for solving hard problems towards building trustworthy and dynamic websites.</p>
           <a href='https://github.com/wowgr8'><BsGithub /></a>
