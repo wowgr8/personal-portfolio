b2dbbea51e5d1d09774e487ae0b87165d9c92226
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 19:10:36 -0700

    add import statement for Link and created links for pages components. Also added button placeholder.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="toggleButton">
+        <button></button>
+      </div>
+      <div className='links'>
+        <Link to='/'> Home </Link>
+        <Link to='/projects'> Projects </Link>
+        <Link to='/experience'> Experience </Link>
+      </div>
+    </div>
   )
 }
 
