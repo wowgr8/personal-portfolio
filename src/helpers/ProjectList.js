8aebee86aed8148c939fd25dd30b1a2315fbb0ad
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-10 13:27:05 -0700

    add imports for project gifs and added project properties to array. (to be imported and looped in Project.js)

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import CAMPY from '../assets/gif/CAMPY.gif';
+import CatFish from '../assets/gif/CatFish.gif';
+
+export const projectList = [
+  {
+    name: "CAMPY",
+    image: CAMPY
+  },
+  {
+    name: "CatFish",
+    image: CatFish
+  }
+]
