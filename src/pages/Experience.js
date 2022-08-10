e416f1ed8a9012d9ee7062c9d4aefe363d97f91f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-10 12:00:23 -0700

    add verticaltimeline import statment. Added styling and placeholder for education section. WIP

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { IoSchoolOutline } from 'react-icons/io5';
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement className="verticle-timeline-element--education" date="2010 - 2014" iconStyle={{ background: "#3e497a", color: "#fff" }} icon={<IoSchoolOutline />}>
+
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
   )
 }
 
