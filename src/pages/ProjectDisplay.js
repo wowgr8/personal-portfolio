ab10d43c728423dee12eb52a49eab03ca6cd7eb1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-10 14:08:10 -0700

    add import statement for styling

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { ProjectList } from '../helpers/ProjectList';
 import { BsGithub } from 'react-icons/bs';
+import '../styles/ProjectDisplay.css';
 
 function ProjectDisplay() {
   const { id } = useParams();
