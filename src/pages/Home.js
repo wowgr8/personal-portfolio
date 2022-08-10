9400f27c1b1958f21ff84a88277038c608035828
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 21:18:02 -0700

    add about me placeholder and icons with links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hello, this is an about me placeholder</h2>
+        <div className='prompt'>
+          <p> A driven Junior web developer pivoting from a career in customer service. Motivated to apply my interpersonal skills and passion for solving hard problems towards building trustworthy and dynamic websites.</p>
+          <a href='https://github.com/wowgr8'><BsGithub /></a>
+          <a href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
+          <a href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
+        </div>
+      </div>
+      <div className='skills'>
+
+      </div>
+    </div>
   )
 }
 
