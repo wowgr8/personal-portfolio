15a05aff001b0379bc990391e8382737684451db
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-09 20:09:36 -0700

    add useState() to handle expanding hamburger menu in navbar for responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import { Divide as Hamburger } from 'hamburger-react';
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar ] = useState(false);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
-        <button>
+        <button onClick={() => {setExpandNavbar((prev) => !prev);}}>
           <Hamburger />
         </button>
       </div>
