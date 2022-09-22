cd015249b2c49d02a9c69319fc5bfd952a324926
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-09-21 23:27:34 -0700

    add proper indentation and unorder lists.

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -15,8 +15,12 @@ function Experience() {
           <h4 className='vertical-timeline-element-subtitle'>
             Portland, Oregon
           </h4>
-          <p> 
-            Meticulously reviewed photo deposits as well as searched for and identified suspicious deposit-to-withdrawal patterns as part of the Deposit Risk team, Supported account holders and applicants via phone and message channels with empathy and high attention to detail, Expertise in balancing multiple streams of work across various platforms including Belvedere, Jira, Confluence, and SalesForce.
+          <p>
+            <ul>
+              <li>Meticulously reviewed photo deposits as well as searched for and identified suspicious deposit-to-withdrawal patterns as part of the Deposit Risk team.</li>
+              <li>Supported account holders and applicants via phone and message channels with empathy and high attention to detail.</li>
+              <li>Expertise in balancing multiple streams of work across various platforms including Belvedere, Jira, Confluence, and SalesForce.</li>
+            </ul>
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement className="vertical-timeline-element--work" date="04/2020 - 06/2021" iconStyle={{ background: "#e9d35b", color: "#fff" }} icon={<GiSuitcase />}>
@@ -26,13 +30,15 @@ function Experience() {
           <h4 className='vertical-timeline-element-subtitle'>
             Portland, Oregon
           </h4>
-          <p><ul>
-            <li>Identify and propose creative solutions for workflow gaps.</li>
-            <li>Assisted with new hire training and shadowing sessions.</li>
-            <li>Researched and responded to regulatory complaints from the BBB and CFPB.</li>
-            <li>Liaised with internal departments to thoroughly research and resolve issues and inquiries.</li>
-            <li>Secured and replaced compromised accounts.</li>
-          </ul></p>
+          <p>
+            <ul>
+              <li>Identify and propose creative solutions for workflow gaps.</li>
+              <li>Assisted with new hire training and shadowing sessions.</li>
+              <li>Researched and responded to regulatory complaints from the BBB and CFPB.</li>
+              <li>Liaised with internal departments to thoroughly research and resolve issues and inquiries.</li>
+              <li>Secured and replaced compromised accounts.</li>
+            </ul>
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement className="vertical-timeline-element--education" date="10/2021 - 08/2022" iconStyle={{ background: "#3e497a", color: "#fff" }} icon={<IoSchoolOutline />} >
           <h3 className='vertical-timeline-element-title'>
