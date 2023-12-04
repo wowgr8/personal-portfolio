414f033191ba3f65760ae4d3fd9499a6f06a7d05
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-03 16:57:12 -0800

    remove "personal"

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ import '../styles/Projects.css';
 function Projects() {
   return (
     <div className='projects'>
-      <h1> My Personal Projects</h1>
+      <h1> My Projects</h1>
       <div className='projectList'>
         {ProjectList.map((project, index) => {
           return <ProjectItem id={index} name={project.name} image={project.image} />;
