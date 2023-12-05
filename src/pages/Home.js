b51116751ebb5409090d8e9e4d6e85791346a5ee
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-05 15:08:07 -0800

    remove CSS and add TailwindCSS styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
-import '../styles/Home.css';
 
 function Home() {
   return (
-    <div className=' w-full items-center font-sans text-blue-800'>
+    <div className='w-full items-center font-sans text-blue-800'>
       <div className='about w-full h-screen flex flex-col justify-center items-center text-center bg-blue-800 text-white'>
         <h2 className="text-7xl text-yellow-400 h-24 pb-4">Howdy, I'm Cesar</h2>
         <div className='w-70 text-4xl px-8'>
-          <p> A driven Junior web developer pivoting from a career in customer service. Motivated to apply my interpersonal skills and passion for solving hard problems towards building trustworthy and dynamic websites.</p>
+          <p className="lg:text-2xl">A driven Junior web developer pivoting from a career in customer service. Motivated to apply my interpersonal skills and passion for solving hard problems towards building trustworthy and dynamic websites.</p>
           <div className='flex items-center justify-center mt-5'>
             <a className="text-5xl m-7 text-white" href='https://github.com/wowgr8'><BsGithub /></a>
             <a className="text-5xl m-7 text-white" href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
@@ -17,20 +16,20 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className='skills'>
-        <h1> Skills</h1>
-        <ol className='list'>
+      <div className='skills text-3xl w-full flex justify-center flex-col items-center'>
+        <h1 className="text-blue-800">Skills</h1>
+        <ol className='list list-none lg:w-60%'>
           <li className='item'>
-            <h2>Front-End</h2>
-            <span> ReactJS, Redux, BootStrap, TailwindCSS, MaterialUI, NPM, StyledComponents, Webpack, CSS, HTML, jQuery.</span>
+            <h2 className="text-2xl">Front-End</h2>
+            <span className="text-base"> ReactJS, Redux, BootStrap, TailwindCSS, MaterialUI, NPM, StyledComponents, Webpack, CSS, HTML, jQuery.</span>
           </li>
           <li className='item'>
-            <h2>Back-End</h2>
-            <span> MySQL/MySQLWorkbench, NodeJS, Firebase, .NET, Entity Framework/Core, Jest, Postman.</span>
+            <h2 className="text-2xl">Back-End</h2>
+            <span className="text-base"> MySQL/MySQLWorkbench, NodeJS, Firebase, .NET, Entity Framework/Core, Jest, Postman.</span>
           </li>
           <li className='item'>
-            <h2>Languages</h2>
-            <span> JavaScript, C#</span>
+            <h2 className="text-2xl">Languages</h2>
+            <span className="text-base"> JavaScript, C#</span>
           </li>
         </ol>
       </div>
