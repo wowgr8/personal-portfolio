a1f2c05701f4d59e032d74a161297f7f0fa8ef17
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-04 23:45:09 -0800

    Add TailwindCSS styling to social media icons - WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,16 @@ import '../styles/Home.css';
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='about'>
-        <h2>Howdy, I'm Cesar</h2>
-        <div className='prompt'>
+    <div className=' w-full items-center font-sans text-blue-800'>
+      <div className='about w-full h-screen flex flex-col justify-center items-center text-center bg-blue-800 text-white'>
+        <h2 className="text-7xl text-yellow-400 h-24 pb-4">Howdy, I'm Cesar</h2>
+        <div className='w-70 text-4xl px-8'>
           <p> A driven Junior web developer pivoting from a career in customer service. Motivated to apply my interpersonal skills and passion for solving hard problems towards building trustworthy and dynamic websites.</p>
-          <a href='https://github.com/wowgr8'><BsGithub /></a>
-          <a href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
-          <a href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
+          <div className='flex items-center justify-center mt-5'>
+            <a className="text-5xl m-7 text-white" href='https://github.com/wowgr8'><BsGithub /></a>
+            <a className="text-5xl m-7 text-white" href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
+            <a className="text-5xl m-7 text-white" href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
+          </div>
         </div>
       </div>
       <div className='skills'>
