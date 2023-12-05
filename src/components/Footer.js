cd7ec8e6d42603cb8678f2a99f274f24ce1c73a0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-05 15:15:11 -0800

    add tailwindCSS styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,21 @@ import '../styles/Footer.css'
 
 function Footer() {
   return (
-    <div className='footer'>
-      <div className='socialMedia'>
-        <a href='https://github.com/wowgr8'><BsGithub /></a>
-        <a href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
-        <a href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
+    <div className="w-full h-250 bg-blue-800 flex flex-col justify-center items-center pt-12 mt-250">
+      <div className="flex">
+        <a href="https://github.com/wowgr8" className="text-white m-6 text-5xl">
+          <BsGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/cesar-aug-lopez/" className="text-white m-6 text-5xl">
+          <BsLinkedin />
+        </a>
+        <a href="mailto:lopez.cesar.aug@gmail.com" className="text-white m-6 text-5xl">
+          <AiOutlineMail />
+        </a>
       </div>
-      <p> &copy; 2022 github.com/wowgr8</p>
+      <p className="text-white text-xl">&copy; 2022 github.com/wowgr8</p>
     </div>
+
   )
 }
 
