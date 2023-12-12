d7855a92b6e3088b1f89b7eb69722fa630dbeb93
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-11 16:37:00 -0800

    replace react-router-dom Link with react-scroll Link, remove CSS styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import '../styles/Navbar.css';
+// import { Link, useLocation } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
 import { Divide as Hamburger } from 'hamburger-react';
 
 function Navbar() {
 
   const [expandNavbar, setExpandNavbar ] = useState(false);
-  const location = useLocation();
+  // const location = useLocation();
 
-  useEffect(()=> {
-    setExpandNavbar(false)
-  },[location])
+  // useEffect(()=> {
+  //   setExpandNavbar(false)
+  // },[location])
 
   return (
-    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+    <main className="navbar bg-red-300 " id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
-        <button onClick={() => {setExpandNavbar((prev) => !prev);}}>
+        {/* <button onClick={() => {setExpandNavbar((prev) => !prev);}}>
           <Hamburger toggled={expandNavbar} toggle={location} />
-        </button>
+        </button> */}
       </div>
-      <div className='links'>
-        <Link to='/'> Home </Link>
-        <Link to='/projects'> Projects </Link>
-        <Link to='/experience'> Experience </Link>
-      </div>
-    </div>
+
+      <ul className="flex">
+        <li className="mr-6">
+          <ScrollLink to="home" smooth={true} duration={500}>
+            Home
+          </ScrollLink>
+        </li>
+        <li className="mr-6">
+          <ScrollLink to="projects" smooth={true} duration={500}>
+            Projects
+          </ScrollLink>
+        </li>
+        <li className="mr-6">
+          <ScrollLink to="experience" smooth={true} duration={500}>
+            Experience
+          </ScrollLink>
+        </li>
+      </ul>
+    </main>
   )
 }
 
 export default Navbar
+
+      {/* <div className='links'>
+        <Link to='/'> Home </Link>
+        <Link to='/projects'> Projects </Link>
+        <Link to='/experience'> Experience </Link>
+      </div> */}
