f27390ffa1dbe53a24aed7e165d93d0a3ea14efd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-12 14:42:48 -0800

    add bug fix-replaced hamburger icon with react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
-import { Divide as Hamburger } from 'hamburger-react';
+import { FaBars } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
+
+
+
 import Logo from '../assets/img/logo-yellow-cad038.png'
 
 const mobileMenuLinks = [
@@ -21,36 +25,29 @@ const mobileMenuLinks = [
 function Navbar() {
   const [expandNavbar, setExpandNavbar ] = useState(false);
 
-  // const handleNavBarClose = () => {
-  //   setExpandNavbar(false);
-  // };
-
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-blue-950" > 
       <div className="flex container lg-py-4  flex-wrap items-center justify-between mx-auto px-4 py-2 ">
         <ScrollLink to="home" smooth={true} duration={500}>
           <img src={Logo} alt="Logo" width={160} height={40} />
         </ScrollLink>
-
         <div className=" mobile-menu block md:hidden">
           {!expandNavbar ? (
             <button
               onClick={() => setExpandNavbar(true)}
               className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white text-slate-200"
             >
-              <Hamburger className=" h-5 w-5" />
+              <FaBars className=" h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setExpandNavbar(false)}
               className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white text-slate-200"
             >
-              <Hamburger className="h-5 w-5" />
+              <IoCloseSharp className="h-5 w-5" />
             </button>
           )}
         </div>
-
-
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex py-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 ">
             <li className="mr-6 hover:text-white">
@@ -71,9 +68,6 @@ function Navbar() {
           </ul>
         </div>
       </div>
-
-
-
       {expandNavbar ? (
         <ul className="flex flex-col py-4 items-center text-[#ADB7BE]">
           {" "}
