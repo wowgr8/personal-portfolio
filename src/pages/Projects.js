a058cfb0573650f44b27441d998ecbe63497970b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-11 20:48:08 -0800

    add Element tag and key property to loop

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Element } from 'react-scroll';
 import ProjectItem from '../components/ProjectItem';
 import { ProjectList } from '../helpers/ProjectList';
 import '../styles/Projects.css';
@@ -6,14 +7,16 @@ import '../styles/Projects.css';
 
 function Projects() {
   return (
-    <div className='projects'>
-      <h1 className='font-bold text-6xl mb-12'> My Projects</h1>
-      <div className='projectList'>
-        {ProjectList.map((project, index) => {
-          return <ProjectItem id={index} name={project.name} image={project.image} />;
-        })}
+    <Element name="projects">
+      <div className='projects'>
+        <h1 className='font-bold text-6xl mb-12'> My Projects</h1>
+        <div className='projectList'>
+          {ProjectList.map((project, index) => {
+            return <ProjectItem key={index} name={project.name} image={project.image} />;
+          })}
+        </div>
       </div>
-    </div>
+    </Element>
   )
 }
 
