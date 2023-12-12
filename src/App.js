644ce17c032c781a77edd88a2267fb3955605b63
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-11 16:34:10 -0800

    remove Routes and Route - for react-scroll in Navbar.js

--- src/App.js
+++ src/App.js
@@ -5,24 +5,31 @@ import Experience from './pages/Experience';
 import Projects from './pages/Projects';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import ProjectDisplay from './pages/ProjectDisplay';
+// import ProjectDisplay from './pages/ProjectDisplay';
 import '../src/App.css'
 
 function App() {
   return (
-    <div className="w-full h-auto flex flex-col font-sans  bg-blue-950">
+    <main className="flex min-h-screen flex-col  bg-blue-950">
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
-          <Route path="/experience" element={<Experience />} />
-        </Routes>
+        <div className="container mt-24 mx-auto px-12 py-4" >
+          <Home />
+        
+          <Projects />
+          <Experience />
+        </div>
         <Footer />
       </Router>
-    </div>
+    </main>
   );
 }
 
 export default App;
+
+        {/* <Routes className="container mt-24 mx-auto px-12 py-4">
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes> */}
