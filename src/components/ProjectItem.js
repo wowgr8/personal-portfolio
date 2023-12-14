3390d76941ab2a12fba48f45444d5591676b6e13
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-14 14:14:10 -0800

    moved to ProjectDisplay.js

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useNavigate } from "react-router-dom";
+// import React from 'react';
+// // import { useNavigate } from "react-router-dom";
 
-function ProjectItem({ image, name, id }) {
-  const navigate = useNavigate();
-  return (
-    <div className='projectItem' onClick={()=> {navigate("/project/" + id);}} >
-      <div style={{backgroundImage: `url(${image})`}} className='bgImage'/>
-      <h1> { name } </h1>
-    </div>
-  )
-}
+// function ProjectItem({ image, name, id }) {
+//   // const navigate = useNavigate();
+//   return (
+//     <div className='h-52 md:-h-72 rounded-t-xl relative group' style={{backgroundImage: `url(${image})`, backgroundSize: "cover"}}  >
+//       {/* <div  className='bgImage'/> */}
+//       <h1> { name } </h1>
+//     </div>
+//   )
+// }
 
-export default ProjectItem;
+// export default ProjectItem;
