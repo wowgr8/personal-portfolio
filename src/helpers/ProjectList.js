283c97469462b41d746b028a6765489f4489f5e1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-14 12:30:01 -0800

    add 'id' key property to object

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -7,36 +7,42 @@ import phrazel from '../assets/gif/phrazel-demo1.gif';
 
 export const ProjectList = [
   {
+    id: 1,
     name: "Opine School",
     image: OpineSchool,
     skills: "ReactJS, JavaScript/JSX, Material-UI, Node.js, HTML",
     link: "https://opineschool.com/"
   },
   {
+    id: 2,
     name: "Phrazel",
     image: phrazel,
     skills: "ReactJS, JavaScript/JSX, Websockets, TailwindCSS, Node.js, Express, HTML",
     link: "https://github.com/wowgr8/phrazel-frontend"
   },
   {
+    id: 3,
     name: "Habitually",
     image: Habitually,
     skills: "ReactJS, Firebase, JavaScript/JSX, Node.js, HTML, CSS, TailwindCSS, PokeAPI",
     link: "https://github.com/wowgr8/habitually"
   },
   {
+    id: 4,
     name: "CAMPY",
     image: CAMPY,
     skills: "JavaScript, Jest, Node.js, HTML, CSS, Bootstrap, jQuery, OpenWeatherAPI, RecipeAPI",
     link: "https://github.com/wowgr8/camp-planner"
   },
   {
+    id: 5,
     name: "CatFish",
     image: CatFish,
     skills: "C#, .NET, MySql/Workbench, Bootstrap, HTML, jQuery, Animate.css",
     link: "https://github.com/wowgr8/Catfish.Solution"
   },
   {
+    id: 6,
     name: "Star Wars: CodeTheDream",
     image: SWAPI,
     skills: "ReactJS, JavaScript/JSX, Node.js, HTML, TailwindCSS, StarWarsAPI (SWAPI)",
