2fcb4f5c13da9b2410c3e0eaab0000e0c9a67a97
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-14 14:36:52 -0800

    replace commas with dots

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -10,42 +10,42 @@ export const ProjectList = [
     id: 1,
     name: "Opine School",
     image: OpineSchool,
-    skills: "ReactJS, JavaScript/JSX, Material-UI, Node.js, HTML",
+    skills: "ReactJS • JavaScript/JSX • Material-UI • Node.js • HTML",
     link: "https://opineschool.com/"
   },
   {
     id: 2,
     name: "Phrazel",
     image: phrazel,
-    skills: "ReactJS, JavaScript/JSX, Websockets, TailwindCSS, Node.js, Express, HTML",
+    skills: "ReactJS • JavaScript/JSX • Websockets • TailwindCSS • Node.js • Express • HTML",
     link: "https://github.com/wowgr8/phrazel-frontend"
   },
   {
     id: 3,
     name: "Habitually",
     image: Habitually,
-    skills: "ReactJS, Firebase, JavaScript/JSX, Node.js, HTML, CSS, TailwindCSS, PokeAPI",
+    skills: "ReactJS • Firebase • JavaScript/JSX • Node.js • HTML • CSS • TailwindCSS • PokeAPI",
     link: "https://github.com/wowgr8/habitually"
   },
   {
     id: 4,
     name: "CAMPY",
     image: CAMPY,
-    skills: "JavaScript, Jest, Node.js, HTML, CSS, Bootstrap, jQuery, OpenWeatherAPI, RecipeAPI",
+    skills: "JavaScript • Jest • Node.js • HTML • CSS • Bootstrap • jQuery • OpenWeatherAPI • RecipeAPI",
     link: "https://github.com/wowgr8/camp-planner"
   },
   {
     id: 5,
     name: "CatFish",
     image: CatFish,
-    skills: "C#, .NET, MySql/Workbench, Bootstrap, HTML, jQuery, Animate.css",
+    skills: "C# • .NET • MySql/Workbench • Bootstrap • HTML • jQuery • Animate.css",
     link: "https://github.com/wowgr8/Catfish.Solution"
   },
   {
     id: 6,
     name: "Star Wars: CodeTheDream",
     image: SWAPI,
-    skills: "ReactJS, JavaScript/JSX, Node.js, HTML, TailwindCSS, StarWarsAPI (SWAPI)",
+    skills: "ReactJS • JavaScript/JSX • Node.js • HTML • TailwindCSS • StarWarsAPI (SWAPI)",
     link: "https://github.com/wowgr8/star-wars"
   }
 ]
