5a8059c315e8023be6577bb1d6848cc635e54716
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-13 19:33:10 -0800

    add SKILLS_DATA array of skills and education

--- src/pages/AboutSection.js
+++ src/pages/AboutSection.js
@@ -2,8 +2,40 @@ import React from 'react';
 import { Element } from 'react-scroll';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
-import DeskCartoon from '../assets/img/Desk-Cartoon-Vector.png'
-import CartoonComputerBoy from '../assets/img/cartoon-computer-boy.png'
+import DeskCartoon from '../assets/img/Desk-Cartoon-Vector.png';
+import CartoonComputerBoy from '../assets/img/cartoon-computer-boy.png';
+
+const SKILLS_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className=" list-disc pl-2">
+        <li>JavaScript</li>
+        <li>C#</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>MySQLWorkbench</li>
+        <li>Material-UI</li>
+        <li>TailwindCSS</li>
+        <li>Figma</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Epicodus - Fullstack Web Development Bootcamp</li>
+        <li>Code The Dream - Web Development Bootcamp</li>
+      </ul>
+    ),
+  },
+];
 
 function AboutSection() {
   return (
@@ -22,8 +54,7 @@ function AboutSection() {
             Im a driven full-stack Software Engineer with a specialization in frontend development.  Dedicated to crafting dynamic and responsive web applications. My skill set includes JavaScript, C#, React, Redux, Node.js, Express, SQL, mySQL Workbench, Postman, Figma, Material-UI, TailwindCSS, HTML, CSS, and Git. I look forward to facing new challenges, learning new skills, and building strong and lasting work relationships along the way. I'm enthusiastic about working with fellow professionals to co-create outstanding applications.
           </p>
 
-          {/* <div className='skills text-3xl w-full flex justify-center flex-col items-center h-[90vh]'>
-        </div> */}
+
           <h1 className="text-blue-800">Skills</h1>
           <ol className='list list-none lg:w-60%'>
             <li className='item'>
