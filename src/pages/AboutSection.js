03a89dd1cbdb509cd0a027272cf43b1f31a92039
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-13 19:20:34 -0800

    add cartoon images - WIP

--- src/pages/AboutSection.js
+++ src/pages/AboutSection.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { Element } from 'react-scroll';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
+import DeskCartoon from '../assets/img/Desk-Cartoon-Vector.png'
+import CartoonComputerBoy from '../assets/img/cartoon-computer-boy.png'
 
 function AboutSection() {
   return (
-    <Element name="AboutSection" className='text-white'>
+    <Element name="AboutSection" className='text-white bg-slate-700'>
       <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        {/* <img>placeholder</img> */}
-        <div className='flex items-center justify-center'>
+        <img src={CartoonComputerBoy} width={400} height={400} alt="cartoon web developer"/>
+        {/* <img src={DeskCartoon} width={500} height={500} alt=""/> */}
+        {/* <div className='flex items-center justify-center'>
           <a className="text-5xl m-7 text-white" href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
           <a className="text-5xl m-7 text-white" href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
           <a className="text-5xl m-7 text-white" href='https://github.com/wowgr8'><BsGithub /></a>
-        </div>
+        </div> */}
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base md:text-lg">
             Im a driven full-stack Software Engineer with a specialization in frontend development.  Dedicated to crafting dynamic and responsive web applications. My skill set includes JavaScript, C#, React, Redux, Node.js, Express, SQL, mySQL Workbench, Postman, Figma, Material-UI, TailwindCSS, HTML, CSS, and Git. I look forward to facing new challenges, learning new skills, and building strong and lasting work relationships along the way. I'm enthusiastic about working with fellow professionals to co-create outstanding applications.
           </p>
 
-          <div className='skills text-3xl w-full flex justify-center flex-col items-center h-[90vh]'>
-        </div>
+          {/* <div className='skills text-3xl w-full flex justify-center flex-col items-center h-[90vh]'>
+        </div> */}
           <h1 className="text-blue-800">Skills</h1>
           <ol className='list list-none lg:w-60%'>
             <li className='item'>
