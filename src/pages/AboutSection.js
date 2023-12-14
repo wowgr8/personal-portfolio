5716c6a1239b572ba131108024ff454f245b180e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-14 12:18:27 -0800

    add handleTabChange handler fn, setTab useState, import SkillsButton.js

--- src/pages/AboutSection.js
+++ src/pages/AboutSection.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState, useTransition} from 'react';
 import { Element } from 'react-scroll';
-import { BsGithub, BsLinkedin } from 'react-icons/bs';
-import { AiOutlineMail } from 'react-icons/ai';
-import DeskCartoon from '../assets/img/Desk-Cartoon-Vector.png';
+// import { BsGithub, BsLinkedin } from 'react-icons/bs';
+// import { AiOutlineMail } from 'react-icons/ai';
+// import DeskCartoon from '../assets/img/Desk-Cartoon-Vector.png';
 import CartoonComputerBoy from '../assets/img/cartoon-computer-boy.png';
+import SkillsButton from '../components/SkillsButton';
 
 const SKILLS_DATA = [
   {
@@ -22,6 +23,8 @@ const SKILLS_DATA = [
         <li>Material-UI</li>
         <li>TailwindCSS</li>
         <li>Figma</li>
+        <li>MongoDB</li>
+        <li>Firebase</li>
       </ul>
     ),
   },
@@ -38,24 +41,61 @@ const SKILLS_DATA = [
 ];
 
 function AboutSection() {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
   return (
     <Element name="AboutSection" className='text-white bg-slate-700'>
       <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
         <img src={CartoonComputerBoy} width={400} height={400} alt="cartoon web developer"/>
         {/* <img src={DeskCartoon} width={500} height={500} alt=""/> */}
-        {/* <div className='flex items-center justify-center'>
-          <a className="text-5xl m-7 text-white" href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
-          <a className="text-5xl m-7 text-white" href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
-          <a className="text-5xl m-7 text-white" href='https://github.com/wowgr8'><BsGithub /></a>
-        </div> */}
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base md:text-lg">
-            Im a driven full-stack Software Engineer with a specialization in frontend development.  Dedicated to crafting dynamic and responsive web applications. My skill set includes JavaScript, C#, React, Redux, Node.js, Express, SQL, mySQL Workbench, Postman, Figma, Material-UI, TailwindCSS, HTML, CSS, and Git. I look forward to facing new challenges, learning new skills, and building strong and lasting work relationships along the way. I'm enthusiastic about working with fellow professionals to co-create outstanding applications.
+            Im a driven full-stack Software Engineer with a specialization in frontend development.  Dedicated to crafting dynamic and responsive web applications. My skill set includes JavaScript, C#, React, Redux, Node.js, Express, SQL, mySQL Workbench, Postman, Figma, Material-UI, TailwindCSS, HTML, CSS, and Git. I'm excited to face new challenges, learn new skills, and foster strong and lasting work relationships along the way.
           </p>
 
 
-          <h1 className="text-blue-800">Skills</h1>
+          <div className="flex flex-row mt-8">
+            <SkillsButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </SkillsButton>
+            <SkillsButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </SkillsButton>
+          </div>
+          <div className="mt-8 ">
+            {SKILLS_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </Element>
+  )
+}
+
+export default AboutSection
+
+
+
+{/* <div className='flex items-center justify-center'>
+  <a className="text-5xl m-7 text-white" href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
+  <a className="text-5xl m-7 text-white" href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
+  <a className="text-5xl m-7 text-white" href='https://github.com/wowgr8'><BsGithub /></a>
+</div> */}
+
+{/* <h1 className="text-blue-800">Skills</h1>
           <ol className='list list-none lg:w-60%'>
             <li className='item'>
               <h2 className="text-2xl">Front-End</h2>
@@ -69,11 +109,4 @@ function AboutSection() {
               <h2 className="text-2xl">Languages</h2>
               <span className="text-base"> JavaScript, C#</span>
             </li>
-          </ol>
-        </div>
-      </div>
-    </Element>
-  )
-}
-
-export default AboutSection
+          </ol> */}
