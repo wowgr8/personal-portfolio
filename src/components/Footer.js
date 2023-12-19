811f7a4de7489c5b3d3f5f0d93e6c2a5202cac8d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-18 18:39:44 -0800

    update icon color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,17 @@ function Footer() {
   return (
     <div className="w-full h-250 bg-blue-950 flex flex-col justify-center items-center pt-12 mt-250">
       <div className="flex">
-        <a href="https://github.com/wowgr8" className="text-white m-7 text-5xl">
+        <a href="https://github.com/wowgr8" className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl">
           <BsGithub />
         </a>
-        <a href="https://www.linkedin.com/in/cesar-aug-lopez/" className="text-white m-7 text-5xl">
+        <a href="https://www.linkedin.com/in/cesar-aug-lopez/" className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl">
           <BsLinkedin />
         </a>
-        <a href="mailto:lopez.cesar.aug@gmail.com" className="text-white m-7 text-5xl">
+        <a href="mailto:lopez.cesar.aug@gmail.com" className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl">
           <AiOutlineMail />
         </a>
       </div>
-      <p className="text-white text-xl">&copy; 2022 github.com/wowgr8</p>
+      <p className="text-white text-md">&copy; 2022 github.com/wowgr8</p>
     </div>
 
   )
