e9de0126bce5593cd36ce0e8ebc08b9942002e91
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-26 22:01:58 -0800

    remove background opacity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
   ];
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-blue-950" style={{ backgroundColor: 'rgba(17, 24, 39, 0.9)' }} > 
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-blue-950" > 
       <div className="flex container lg-py-4  flex-wrap items-center justify-between mx-auto px-4 py-2 ">
         <ScrollLink to="home" smooth={true} duration={500}>
           <img src={Logo} alt="Logo" width={160} height={40} />
