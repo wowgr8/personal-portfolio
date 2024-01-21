e5fd4cb5c387d82f1744cee863694ee8e2af234f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-20 21:54:26 -0800

    add silicone valley headshot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Element } from 'react-scroll';
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import ProfilePic from "../assets/img/portfoliopic.png"
+import ProfilePic2 from "../assets/img/siliconevalleyone.png"
 import ResumeUrl from "../assets/pdf/CesarLopez-SoftwareEngineer-Resume.pdf"
 
 function Home() {
@@ -63,7 +64,7 @@ function Home() {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <img
-              src={ProfilePic}
+              src={ProfilePic2}
               alt="hero-image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2  sm:top-1/2 left-1/2 lg:top-[55%] lg:w-[300px] lg:h-[400px] w-[200px] h-[260px] top-[55%]"
             />
