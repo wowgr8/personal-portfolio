b9fd2e0ccef224f30dcabf009ef6204e88a16e5e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-22 21:24:50 -0800

    add profile image renamed import path

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Element } from 'react-scroll';
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-import ProfilePic2Cropped from "../assets/img/siliconevalleyoneCropped.png"
+import ProfilePic from "../assets/img/siliconeForestCropped.png"
 import ResumeUrl from "../assets/pdf/CesarLopez-SoftwareEngineer-Resume.pdf"
 
 function Home() {
@@ -63,7 +63,7 @@ function Home() {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <img
-              src={ProfilePic2Cropped}
+              src={ProfilePic}
               alt="hero-image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2  sm:top-1/2 left-1/2 lg:top-[65%] lg:w-[500px] lg:h-[650px] w-[400px] h-[360px] top-[60%]"
             />
