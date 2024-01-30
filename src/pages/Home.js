c2c6cbd927d8f24a9c72192495d6d1309e710f7c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-29 19:01:48 -0800

    replace profile pic with HD pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Element } from 'react-scroll';
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import ProfilePic from "../assets/img/siliconeForestCropped.png"
+import HDPorfolioPic from "../assets/img/hd-portfolio-pic.png"
 import ResumeUrl from "../assets/pdf/CesarLopez-SoftwareEngineer-Resume.pdf"
 
 function Home() {
@@ -63,7 +64,7 @@ function Home() {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <img
-              src={ProfilePic}
+              src={HDPorfolioPic}
               alt="hero-image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2  sm:top-1/2 left-1/2 lg:top-[65%] lg:w-[500px] lg:h-[650px] w-[400px] h-[360px] top-[60%]"
             />
