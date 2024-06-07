d4a24a4c2d9b3f41dff26508ddf89981f513cb8e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-07 12:33:56 -0700

    add typo fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { Element } from 'react-scroll';
+import React from "react";
+import { Element } from "react-scroll";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-import HDPorfolioPic from "../assets/img/hd-portfolio-pic.png"
-import ResumeUrl from "../assets/pdf/CesarLopez-SoftwareEngineer-Resume.pdf"
+import HDPortfolioPic from "../assets/img/hd-portfolio-pic.png";
+import ResumeUrl from "../assets/pdf/CesarLopez-SoftwareEngineer-Resume.pdf";
 
 function Home() {
-
   const handleDownloadResume = () => {
     const link = document.createElement("a");
     link.href = ResumeUrl;
     link.download = "CesarLopez-SoftwareEngineer-Resume.pdf";
     link.click();
-  }
-  
+  };
+
   return (
-    <Element name="home" className='lg:py-16'>
+    <Element name="home" className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -24,7 +23,9 @@ function Home() {
           className="col-span-7 place-self-center text-center sm:text-left"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5lg lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text h-24 pb-4 bg-gradient-to-r from-yellow-300 to-yellow-600">Howdy, I'm</span>
+            <span className="text-transparent bg-clip-text h-24 pb-4 bg-gradient-to-r from-yellow-300 to-yellow-600">
+              Howdy, I'm
+            </span>
             <br></br>
             <TypeAnimation
               sequence={[
@@ -40,19 +41,21 @@ function Home() {
               wrapper="span"
               speed={50}
               repeat={Infinity}
-              />
+            />
           </h1>
-          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-            Welcome to my portfolio! Dive into my experience as a Software Engineer and Developer, highlighting skills across a diverse set of web development technologies.
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Welcome to my portfolio! Dive into my experience as a Software
+            Engineer and Developer, highlighting skills across a diverse set of
+            web development technologies.
           </p>
           <button
-              onClick={handleDownloadResume}
-              className={`px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-600 via-blue-400 to-blue-300 hover:bg-slate-800 text-white mt-3`}
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </button>
+            onClick={handleDownloadResume}
+            className={`px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-600 via-blue-400 to-blue-300 hover:bg-slate-800 text-white mt-3`}
+          >
+            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Download CV
+            </span>
+          </button>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5, x: 400 }}
@@ -63,7 +66,7 @@ function Home() {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <img
-              src={HDPorfolioPic}
+              src={HDPortfolioPic}
               alt="hero-image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2  sm:top-1/2 left-1/2 lg:top-[68%] top-[65%] md:top-[65%] lg:w-[400px] lg:h-[650px] md:w-[250px] md:h-[400px] w-[400px] h-[360px]"
             />
@@ -71,7 +74,7 @@ function Home() {
         </motion.div>
       </div>
     </Element>
-  ) 
+  );
 }
 
-export default Home
+export default Home;
